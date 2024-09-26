4d28d2c499fa89debb312088f4210b89d8b801e3
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2024-09-26 12:19:50 -0700

    Pushed from Domino: https://theme6-demo.workbench-analyze-team-sandbox.domino.tech/workspace/integration-test/base-project?executionId=66f5b34d1f42c328b94fb693

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
-console.log('seba');
+console.log('seb2a');
 
 // https://vitejs.dev/config/
 export default defineConfig({
